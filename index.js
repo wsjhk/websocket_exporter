7d13946ee8e050677b61d0494b44185b88234657
wsjhk <2621570425@qq.com>
2020-04-07 12:48:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ const client = require('prom-client');
 const { WebSocketClient } = require('./websocket');
 const ENDPOINT = process.env.ENDPOINT
 
+var arr = ENDPOINT.toString().split(",");
+
 const gauge = new client.Gauge({
   name: 'websocket',
   help: 'websocket_help',
-  labelNames: ['status']
+  labelNames: ['url', 'status']
 });
 
-const ws = new WebSocketClient();
-ws.open(ENDPOINT);
-ws.onopen = function (e) {
-  gauge.labels('status').set(1);
+for (let index in arr) {
+  const ws = new WebSocketClient();
+  ws.open(arr[index]);
+  ws.onopen = function (e) {
+    gauge.labels(arr[index], 'ok').set(1);
+  }
+  ws.onerror = function (e) {
+    gauge.labels(arr[index], 'error').set(0);
+  }
 }
-ws.onerror = function (e) {
-  gauge.labels('status').set(0);
-}
-
 server.get('/metrics', (req, res) => {
   res.end(client.register.metrics());
 });
